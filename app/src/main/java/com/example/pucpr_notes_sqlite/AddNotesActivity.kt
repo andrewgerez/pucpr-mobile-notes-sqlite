92063633b8c582c486f994388738d521f36ed6b7
Andrew Gerez <andrewgerez18@gmail.com>
2024-07-08 21:56:37 -0300

    feat: layout xml activities

---
+++ app/src/main/java/com/example/pucpr_notes_sqlite/AddNotesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pucpr_notes_sqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notes)
+    }
+}
